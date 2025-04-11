6791f7333abd869211ff4f79ac30c6711ae99d79
nois1 <zionzion00@naver.com>
2025-04-11 16:53:03 +0900

    connect 호출 삭제

--- server/db.js
+++ server/db.js
@@ -13,13 +13,4 @@ const db = mysql.createPool({
   queueLimit: 0
 });
 
-// MySQL 연결
-db.connect((err) => {
-  if (err) {
-    console.error('MySQL 연결 오류:', err);
-    return;
-  }
-  console.log('MySQL에 연결되었습니다.');
-});
-
 module.exports = db; // db 객체를 내보냄
