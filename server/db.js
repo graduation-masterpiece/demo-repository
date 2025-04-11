f150c753c8bdad6a00da2517e1dc739eab437a2c
nois1 <zionzion00@naver.com>
2025-04-11 16:48:43 +0900

    DB 연결 커넥션 풀 사용

--- server/db.js
+++ server/db.js
@@ -1,14 +1,16 @@
 require('dotenv').config(); // dotenv 패키지 로드
 
-// db.js
 const mysql = require('mysql2');
 
 // MySQL 연결 설정
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
 // MySQL 연결
