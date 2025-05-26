fb56fee26203802a55d62a2db5906fc8ee6fee96
Derman_e <71058265+harry1725@users.noreply.github.com>
2025-05-26 15:26:35 +0900

    UTM 로그 버그 수정

--- src/components/UTMLogger.js
+++ src/components/UTMLogger.js
@@ -1,15 +1,17 @@
 import { useEffect } from 'react';
+import axios from 'axios';
 
 function useUTMLogger() {
   useEffect(() => {
     const urlParams = new URLSearchParams(window.location.search);
+    const accessTime = new Date().toISOString();
     
     let utmData = {
       source: urlParams.get('utm_source'),
       medium: urlParams.get('utm_medium'),
       campaign: urlParams.get('utm_campaign'),
       content: urlParams.get('utm_content'),
-      access_time: new Date().toISOString(),
+      access_time: accessTime,
     };
 
     // 만약 링크 직접 입력 또는 메인 페이지로 들어왔을 때
@@ -21,7 +23,7 @@ function useUTMLogger() {
         medium: 'none',
         campaign: 'direct-access',
         content: 'null',
-        access_time: new Date().toISOString(),
+        access_time: accessTime,
       };
     }
 
@@ -29,25 +31,32 @@ function useUTMLogger() {
     const isValid = Object.values(utmData).every(val => typeof val === 'string' && val.length > 0);
 
     // UTM이 있을 경우에만 백엔드로 전송
-    if (isValid && !sessionStorage.getItem('utm_logged')) {
-      console.log("Sending UTM: ", utmData);
-      
-      fetch('/api/log-utm', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(utmData),
-        credentials: 'include',
-      }).then(() => {
+    if (!isValid || sessionStorage.getItem('utm_logged')) return;
+    
+    console.log("Sending UTM: ", utmData);
+
+    const loggingUtm = async () => {
+      try {
+        await axios.post('/api/log-utm', {
+          source: utmData.source,
+          medium: utmData.medium,
+          campaign: utmData.campaign,
+          content: utmData.content,
+          access_time: utmData.access_time,
+        });
+        
         sessionStorage.setItem('utm_logged', 'true');
         sessionStorage.setItem('utm_source', utmData.source || '');
         sessionStorage.setItem('utm_medium', utmData.medium || '');
         sessionStorage.setItem('utm_campaign', utmData.campaign || '');
         sessionStorage.setItem('utm_content', utmData.content || '');
         sessionStorage.setItem('utm_access_time', utmData.access_time || '');
-      }).catch((err) => {
+      } catch(err) {
         console.error("UTM Logging Failed: ", err);
-      });
-    }
+      }
+    };
+
+    loggingUtm();
   }, []);
 }
 
