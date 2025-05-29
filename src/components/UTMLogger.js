4ccb7513861f9abf50a99298f46d70739d374ab3
Derman_e <71058265+harry1725@users.noreply.github.com>
2025-05-30 04:08:22 +0900

    시간 단순화

--- src/components/UTMLogger.js
+++ src/components/UTMLogger.js
@@ -4,14 +4,13 @@ import axios from 'axios';
 function useUTMLogger() {
   useEffect(() => {
     const urlParams = new URLSearchParams(window.location.search);
-    const currentTime = new Date().toISOString().slice(0, 19).replace('T', ' ');
     
     let utmData = {
       source: urlParams.get('utm_source') || sessionStorage.getItem('utm_source'),
       medium: urlParams.get('utm_medium') || sessionStorage.getItem('utm_source'),
       campaign: urlParams.get('utm_campaign') || sessionStorage.getItem('utm_source'),
       content: parseInt(urlParams.get('utm_content') || sessionStorage.getItem('utm_content'), 10),
-      access_time: sessionStorage.getItem('utm_access_time') || currentTime,
+      access_time: sessionStorage.getItem('utm_access_time') || new Date(),
     };
 
     // 만약 링크 직접 입력 또는 메인 페이지로 들어왔을 때
@@ -23,7 +22,7 @@ function useUTMLogger() {
         medium: 'none',
         campaign: 'direct-access',
         content: -1,
-        access_time: currentTime,
+        access_time: new Date(),
       };
     }
 
