035c809973ddafeb9dcc44a53e3da2446059bf62
Derman_e <71058265+harry1725@users.noreply.github.com>
2025-05-30 04:20:50 +0900

    시간대 원상복구
    UTC+09:00 -> UTC

--- server/db.js
+++ server/db.js
@@ -8,7 +8,6 @@ const db = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  timezone: '+09:00',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
