77d6e76185bfc6fa2880d06741e4a05470ed2a1f
Derman_e <71058265+harry1725@users.noreply.github.com>
2025-06-01 20:38:16 +0900

    GA4 코드 수정

--- src/App.js
+++ src/App.js
@@ -8,35 +8,6 @@ import { SidebarProvider } from "./SidebarContext";
 import useUTMLogger from "./components/UTMLogger.js";
 
 function App() {
-  useEffect(() => {
-    console.log('Before redirection: ', window.location.href);
-    
-    const params = new URLSearchParams(window.location.search);
-    
-    const source = params.get('utm_source');
-    const medium = params.get('utm_medium');
-    const campaign = params.get('utm_campaign');
-    const content = parseInt(params.get('utm_content'), 10);
-    
-    if (source || medium || campaign || content) {
-      sessionStorage.setItem('utm_source', source || '');
-      sessionStorage.setItem('utm_medium', medium || '');
-      sessionStorage.setItem('utm_campaign', campaign || '');
-      sessionStorage.setItem('utm_content', content || '');
-      sessionStorage.setItem('utm_access_time', new Date());
-    }
-    
-    console.log("Stored utm_source in sessionStorage: ", sessionStorage.getItem('utm_source'));
-
-    if (typeof window.gtag === 'function') {
-      window.gtag('config', 'G-5KXQCF3KEJ', {
-        page_path: window.location.pathname + window.location.search,
-        ...utm,
-      });
-      console.log('Passed UTM to GA4: ', utm);
-    }
-  }, []);
-  
   useUTMLogger();
   
   return (
