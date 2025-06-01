4b60b0ef61864618a6fc778e0199efa4b41910f8
Derman_e <71058265+harry1725@users.noreply.github.com>
2025-06-01 20:25:51 +0900

    GA4 사용 지연

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainPage from "./components/MainPage";
 import MyLibraryPage from "./components/MyLibraryPage";
@@ -8,6 +8,35 @@ import { SidebarProvider } from "./SidebarContext";
 import useUTMLogger from "./components/UTMLogger.js";
 
 function App() {
+  useEffect(() => {
+    console.log('Before redirection: ', window.location.href);
+    
+    const params = new URLSearchParams(window.location.search);
+    
+    const source = params.get('utm_source');
+    const medium = params.get('utm_medium');
+    const campaign = params.get('utm_campaign');
+    const content = parseInt(params.get('utm_content'), 10);
+    
+    if (source || medium || campaign || content) {
+      sessionStorage.setItem('utm_source', source || '');
+      sessionStorage.setItem('utm_medium', medium || '');
+      sessionStorage.setItem('utm_campaign', campaign || '');
+      sessionStorage.setItem('utm_content', content || '');
+      sessionStorage.setItem('utm_access_time', new Date());
+    }
+    
+    console.log("Stored utm_source in sessionStorage: ", sessionStorage.getItem('utm_source'));
+
+    if (typeof window.gtag === 'function') {
+      window.gtag('config', 'G-5KXQCF3KEJ', {
+        page_path: window.location.pathname + window.location.search,
+        ...utm,
+      });
+      console.log('Passed UTM to GA4: ', utm);
+    }
+  }, []);
+  
   useUTMLogger();
   
   return (
